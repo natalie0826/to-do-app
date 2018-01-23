b7e15fe0d8911f9ee41b8956546e04f8521ea055
Natallia Chobat <natallia.chobat@itechart-group.com>
2018-01-23 15:34:19 +0300

    Started to-app (create, delete, complete)

---
+++ src/containers/FilterLink.js
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux'
+import { setFilter } from '../actions'
+import Link from '../components/Link'
+
+const mapStateToProps = (state, ownProps) => ({
+  active: ownProps.filter === state.filter
+})
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onClick: () => {
+    dispatch(setFilter(ownProps.filter))
+  }
+})
+
+const FilterLink = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Link)
+
+export default FilterLink
