7d249dde426c186fb3932fb04f9e1c311cc74c6c
natallia.chobat <natashkachobot@mail.ru>
2018-02-14 21:32:54 +0300

    Changed App.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { ShowTodoList } from '../containers/ShowTodoList';
-import { FetchAndDisplayData } from '../containers/DataHoc';
-import { AddTodo } from '../containers/AddTodo';
+
+import { FetchAndDisplay } from '../containers/FetchAndDisplay';
 import { configureStore } from '../configureStore';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import '../../node_modules/react-tabs/style/react-tabs.css';
 
 import { AddCategory } from './common/AddCategory';
@@ -14,23 +12,7 @@ export const App = () => {
         <div className="container">
             <AddCategory store={configureStore} categories={configureStore.categories} />
             <h3>Todos</h3>
-            <AddTodo isAddTodo={true} categories={configureStore.categories}/>
-
-            <FetchAndDisplayData />
-            
-            {/* <Tabs>
-                <TabList>
-                    <Tab>All tasks</Tab>
-                    <Tab>Categories</Tab>
-                </TabList>
-            
-                <TabPanel>
-                    <ShowTodoList flag="list"/>
-                </TabPanel>
-                <TabPanel>
-                    <ShowTodoList flag="categories"/>
-                </TabPanel>
-            </Tabs> */}
+            <FetchAndDisplay />
         </div>
     );
 };
