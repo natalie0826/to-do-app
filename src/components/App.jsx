f0795db591c2db56c1568fbfbe62bf9228f0d3ac
natallia.chobat <natashkachobot@mail.ru>
2018-02-19 19:31:45 +0300

    Changed hierarchy of components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import '../../node_modules/react-tabs/style/react-tabs.css';
 
-import { configureStore } from '../configureStore';
 import { withFetching } from '../containers/withFetching';
-import { ButtonToOpenModal } from './common/ButtonToOpenModal';
 import { urls } from '../constants/constantsUrls';
+import { ShowTodoApp } from '../containers/ShowTodoApp';
 import '../styles/todo.css';
 
-import { ShowTodoApp } from '../containers/ShowTodoApp';
-
 export const App = (props) => {
-    const TodoAppWithFetch = withFetching(urls.todos)(ShowTodoApp);
+    const TodoAppWithFetch = withFetching([urls.todos, urls.categories])(ShowTodoApp);
     
     return (
         <div className="container">
-            <ButtonToOpenModal store={configureStore} />
-            <h3>Todos</h3>
             <TodoAppWithFetch />
         </div>
     );
